423306eea45008d4214817f6854344dc28598eb2
Nikolay Topkaridi <enemycnt@gmail.com>
2014-12-18 20:29:40 +0300

    add context for imported and exempt

--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -24,6 +24,12 @@ describe Basket::Basket do
     end
   end
 
+  describe "#is_exemp?" do
+    it "check for exemption" do
+      expect(@basket.is_exemp?("imported box of chocolates")).to be true
+    end
+  end
+
 end
 
 describe Basket::Item do
@@ -52,37 +58,69 @@ describe Basket::Item do
 end
 
 describe Basket::ExemptItem do
-  before :each do
-    @item = Basket::ExemptItem.new 1, "music cd", 14.99
-  end
+  context "usual exempt" do
+    before :each do
+      @item = Basket::ExemptItem.new 1, "music cd", 14.99
+    end
 
-  describe "#new" do
-    it "takes three parameters and returns an ExemptItem object" do
-      expect(@item).to be_an_instance_of Basket::ExemptItem
+    describe "#new" do
+      it "takes three parameters and returns an ExemptItem object" do
+        expect(@item).to be_an_instance_of Basket::ExemptItem
+      end
+    end
+
+    describe "#tax" do
+      it "returns correct tax" do
+        expect(@item.tax).to eq(0.0)
+      end
+    end
+
+    describe "#price" do
+      it "returns price with taxes" do
+        expect(@item.price_gross).to eq(14.99)
+      end
+    end
+
+    describe "#imported?" do
+      it "returns true or false depends of imported status" do
+        expect(@item.imported?).to be false
+      end
+    end
+
+    describe "import_tax" do
+      it "returns imported tax" do
+        expect(@item.import_tax).to eq(0.0)
+      end
     end
   end
 
-  describe "#tax" do
-    it "returns correct tax" do
-      expect(@item.tax).to eq(0.0)
+  context "imported and exempt" do
+    before :each do
+      @item = Basket::ExemptItem.new 1, "box of imported chocolates", 11.25
     end
-  end
 
-  describe "#price" do
-    it "returns price with taxes" do
-      expect(@item.price_gross).to eq(14.99)
+    describe "#tax" do
+      # it "returns correct tax" do
+      #   expect(@item.tax).to eq(0.6)
+      # end
     end
-  end
 
-  describe "#imported?" do
-    it "returns true or false depends of imported status" do
-      expect(@item.imported?).to be false
+    describe "#price" do
+      it "returns price with taxes" do
+        expect(@item.price_gross).to eq(11.85)
+      end
     end
-  end
 
-  describe "import_tax" do
-    it "returns imported tax" do
-      expect(@item.import_tax).to eq(0.0)
+    describe "#imported?" do
+      it "returns true or false depends of imported status" do
+        expect(@item.imported?).to be true
+      end
+    end
+
+    describe "import_tax" do
+      it "returns imported tax" do
+        expect(@item.import_tax).to eq(5)
+      end
     end
   end
 end
