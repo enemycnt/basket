cac0967437657166e4b4bd7b0c540456b2fc9330
Nikolay Topkaridi <enemycnt@gmail.com>
2014-12-18 20:30:47 +0300

    added formating for money and path argument

--- bin/basket.rb
+++ bin/basket.rb
@@ -7,7 +7,7 @@ basket = Basket::Basket.new(ARGV[0])
 basket.items.each do |item|
   $stdout.puts "#{item.amount}, #{item.name}, #{item.price_gross}"
 end
-$stdout.puts "\nSales Taxes: #{basket.sales_taxes}"
-$stdout.puts "Total: #{basket.price_sum}"
+$stdout.puts "\nSales Taxes: #{sprintf('%.2f', basket.sales_taxes)}"
+$stdout.puts "Total: #{sprintf('%.2f',basket.price_sum)}"
 $stdout.puts "\n\nCSV exported to #{basket.export_to_csv.path}"
 $stdout.puts "\nBye"
