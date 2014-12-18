8ba1cd118872d937a3a1a4fc10173f3f52ac5bd8
Nikolay Topkaridi <enemycnt@gmail.com>
2014-12-18 17:16:31 +0300

    console executable

---
+++ bin/basket.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+
+require_relative "../lib/basket"
+
+$stdout.puts "Hello. Lets work\n\n"
+basket = Basket::Basket.new(ARGV[0])
+basket.items.each do |item|
+  $stdout.puts "#{item.amount}, #{item.name}, #{item.price_gross}"
+end
+$stdout.puts "\nSales Taxes: #{basket.sales_taxes}"
+$stdout.puts "Total: #{basket.price_sum}"
+$stdout.puts "\n\nCSV exported to #{basket.export_to_csv.path}"
+$stdout.puts "\nBye"
