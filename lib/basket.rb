3fd16a580d921ebfcad24725caeca71db2212d5b
Nikolay Topkaridi <enemycnt@gmail.com>
2014-12-18 16:17:44 +0300

    add main functionality

--- lib/basket.rb
+++ lib/basket.rb
@@ -5,17 +5,42 @@ module Basket
   # Your code goes here...
   class Basket
     attr_accessor :items
-    def initialize
-      path = '/Users/nick/Projects/basket/data/input1.csv'
+
+    EXEMPT_LIST = %w(book chocolate pill)
+
+    def initialize(path)
+      # path = '/Users/nick/Projects/basket/data/input1.csv'
       arr = CSV.read path, :headers => true, :header_converters => :symbol, :col_sep => ", ", :converters => :all
+      @items = []
       arr.each do |row|
-        puts row.inspect
+        if is_exemp?(row[:product])
+          @items.push ExemptItem.new(row[:quantity], row[:product], row[:price])
+        else
+          @items.push Item.new(row[:quantity], row[:product], row[:price])
+        end
       end
+    end
+
+    def is_exemp?(product_name)
+      result = false
+      words_list = product_name.split(' ')
+      words_list.each do |word|
+        result ||= EXEMPT_LIST.any?{|exemp| exemp.match(word)}
+      end
+      result
+    end
+
+    def sales_taxes
+      @items.inject(0){ |sum, a| sum + a.tax }
+    end
+
+    def price_sum
+      @items.inject(0){ |sum, a| sum + a.price_gross }
     end
   end
 
   class Item
-    attr_accessor :amount, :name, :price
+    attr_accessor :amount, :name, :price, :price_gross, :tax
 
     def initialize(amount, name, price)
       @amount = amount
@@ -31,7 +56,7 @@ module Basket
       (@amount * @price * ((sales_tax + import_tax) / 100)).round(2)
     end
 
-    def price
+    def price_gross
       (@price + tax).round(2)
     end
 
--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -1,7 +1,30 @@
 require_relative 'spec_helper'
 require_relative '../lib/basket'
 
+describe Basket::Basket do
+  before :each do
+    @basket = Basket::Basket.new '/Users/nick/Projects/basket/data/input1.csv'
+  end
 
+  describe "#new" do
+    it "takes path parametr and returns a Basket object" do
+      @basket.should be_an_instance_of Basket::Basket
+    end
+  end
+
+  describe "#sales_taxes" do
+    it "returns sum of taxes" do
+      @basket.sales_taxes.should eq(1.5)
+    end
+  end
+
+  describe "#price_sum" do
+    it "returns sum of gross prices" do
+      @basket.price_sum.should eq(29.83)
+    end
+  end
+
+end
 
 describe Basket::Item do
   before :each do
@@ -9,7 +32,7 @@ describe Basket::Item do
   end
 
   describe "#new" do
-    it "takes three parameters and returns a Item object" do
+    it "takes three parameters and returns an Item object" do
       @item.should be_an_instance_of Basket::Item
     end
   end
@@ -22,7 +45,7 @@ describe Basket::Item do
 
   describe "#price" do
     it "returns price with taxes" do
-      @item.price.should eq(16.49)
+      @item.price_gross.should eq(16.49)
     end
   end
 
@@ -34,7 +57,7 @@ describe Basket::ExemptItem do
   end
 
   describe "#new" do
-    it "takes three parameters and returns a Item object" do
+    it "takes three parameters and returns an ExemptItem object" do
       @item.should be_an_instance_of Basket::ExemptItem
     end
   end
@@ -47,7 +70,19 @@ describe Basket::ExemptItem do
 
   describe "#price" do
     it "returns price with taxes" do
-      @item.price.should eq(14.99)
+      @item.price_gross.should eq(14.99)
+    end
+  end
+
+  describe "#imported?" do
+    it "returns true or false depends of imported" do
+      @item.imported?.should be false
+    end
+  end
+
+  describe "import_tax" do
+    it "returns imported tax" do
+      @item.import_tax.should eq(0.0)
     end
   end
 end
