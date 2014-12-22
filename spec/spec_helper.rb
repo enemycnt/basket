5edf3fe99c6198d9ef37dcdc1b92c30ed726ad57
Nikolay Topkaridi <enemycnt@gmail.com>
2014-12-22 20:04:58 +0300

    added test coverage for code climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require 'simplecov'
 SimpleCov.start do
   add_filter '/spec/'
 end
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
