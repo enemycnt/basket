a0ef96a71df3006f70ba74132dc3718696f6a23f
Nikolay Topkaridi <enemycnt@gmail.com>
2014-12-29 21:09:53 +0300

    add "add_item' method from basket

--- lib/basket/item_collection.rb
+++ lib/basket/item_collection.rb
@@ -4,32 +4,20 @@ module Basket
 
     NAME_POSTFIX = "_exported.csv"
 
-    def initialize(path, basket)
+    def initialize(path)
       @path = path
-      @basket = basket
-    end
-
-    def export_to_csv
-      CSV.open(export_path, "wb") do |csv|
-        add_items csv
-        add_footer csv
-      end
     end
 
     def export_path
       @path.gsub(".csv", NAME_POSTFIX)
     end
 
-    def add_items(csv)
-      self.each { |item| csv << [item.amount, item.name, item.price_gross] }
+    def add_item(row)
+      self.push Item.for(row[:quantity], row[:product], row[:price])
     end
 
-    def add_footer(csv )
-      # Empty row
-      csv << [" ", " "]
-      # Total and sales taxes
-      csv << ["Sales Taxes:", @basket.sales_taxes]
-      csv << ["Total:", @basket.price_sum]
+    def export_items(csv)
+      self.each { |item| csv << [item.amount, item.name, item.price_gross] }
     end
 
   end
